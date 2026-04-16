4b34b5328146ae580ad86c1f0802eb4435f65d17
gruijter <gruijter@hotmail.com>
2026-04-16 22:18:59 +0200

    remove gpioget

--- drivers/rpi/device.js
+++ drivers/rpi/device.js
@@ -39,6 +39,7 @@ class RPiDevice extends Device {
       this.lastPollFastTm = 0;
       this.lastPollSlowTm = 0;
       this.lastHourlyPollTm = 0;
+      this.gpioMethodLogged = false;
       this.settings = { ...this.getSettings() };
       await this.migrate().catch((err) => this.error(err));
       if (this.rpi) await this.rpi.connect();
@@ -215,24 +216,14 @@ class RPiDevice extends Device {
         this.lastPollSlowTm = now;
       }
       if (doFastPoll) { // This block is now skipped if monitorGpio is false or interval is 0
-        // Try the fast, lightweight poll first for levels only.
-        const gpioLevels = await this.rpi.getGPIOLevels();
+        const { states: gpio, method } = await this.rpi.getGPIOStates();
 
-        // If fast poll worked AND we have a cached full state from a previous poll...
-        if (gpioLevels && this.lastGpio) {
-          // Merge the new levels into our cached full state, preserving func/pull settings.
-          Object.keys(this.lastGpio).forEach((key) => {
-            if (gpioLevels[key]) {
-              this.lastGpio[key].level = gpioLevels[key].level;
-            }
-          });
-          await this.updateGpioState(this.lastGpio);
-        } else {
-          // Fallback to the heavy poll if fast poll is unavailable or we have no cache.
-          const gpio = await this.rpi.getGPIOStates();
-          // This call will populate this.lastGpio for the next fast poll attempt.
-          await this.updateGpioState(gpio);
+        if (!this.gpioMethodLogged && method) {
+          this.log(`Active GPIO polling method: '${method}'.`);
+          this.gpioMethodLogged = true;
         }
+
+        await this.updateGpioState(gpio);
         this.lastPollFastTm = now;
       }
       if (doHourlyPoll) {
